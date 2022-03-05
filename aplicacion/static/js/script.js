b234c6466a09216293490d4c8a0507e21ed53333
MARIA JOSE SIMON <mariajose.simon@ikasle.egibide.org>
2022-03-05 18:26:34 +0100

    Albaranes mostrar imagen.

--- aplicacion/static/js/script.js
+++ aplicacion/static/js/script.js
@@ -1,12 +1,29 @@
 const btnDelete = document.querySelectorAll('.btn-delete')
 if(btnDelete){
-   const btnArray= Array.from(btnDelete);
-   btnArray.forEach((btn)=>{
+   const btnArrayDelete= Array.from(btnDelete);
+   btnArrayDelete.forEach((btn)=>{
+       console.log("en eliminar")
     btn.addEventListener('click', (e)=>{
         if(!confirm("Quieres eliminar la imagen? ")){
             e.preventDefault();
+            console.log('Elimnada')
         }
     })
 
    });
 }
+
+/*
+Mostrar imagenes. 
+https://css-tricks.com/how-to-show-images-on-click/ */
+
+document.querySelectorAll(".display_image").forEach((item) => {
+    item.addEventListener("click", (event) => {
+      const href = event.target.getAttribute("href");
+      const newImage = document.createElement("img");
+      event.preventDefault();
+      newImage.setAttribute("src", href);
+      document.body.insertBefore(newImage, event.target);
+      event.target.remove();
+    });
+  });
